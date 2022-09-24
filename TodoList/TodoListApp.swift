3b280646a1c06ab6750b0319b1ad02b7290f504f
Louis Rey <louisreypro@yahoo.com>
2022-09-24 23:31:26 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Louis REY on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
